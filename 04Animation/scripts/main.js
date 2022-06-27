58aa891097e018e91cebb5e5c9a1706472f22af1
Tessa <91220360+tessayeethien@users.noreply.github.com>
2022-06-27 17:53:17 +0200

    Add files via upload

---
+++ 04Animation/scripts/main.js
@@ -0,0 +1,16 @@
+
+
+// changing individual properties with code and using setInterval
+var rotationSpeed = 0.05;
+var myOtherBox = document.getElementById('myOtherBox');
+
+function spin(){
+	myOtherBox.object3D.rotation.x + rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.x);
+
+    myOtherBox.object3D.rotation.y += rotationSpeed;
+	console.log(myOtherBox.object3D.rotation.y);
+}
+
+setInterval(spin, 16); //equivalent to 60 fps
+
